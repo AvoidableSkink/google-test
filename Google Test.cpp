0021d27b8d056a2f98c4459724376eb3f7b3bf22
Kimberly <klindholm7@gmail.com>
2019-10-23 12:35:03 -0600

    this is what we turned in

---
+++ Google Test.cpp
@@ -0,0 +1,12 @@
+﻿// Google Test.cpp : Defines the entry point for the application.
+//
+
+#include "Google Test.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
